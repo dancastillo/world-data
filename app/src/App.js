93bfa87d7df0ebdb0e854cdec0ac86beb09af1e7
dancastillo <dancastillo99@gmail.com>
2020-08-28 12:58:07 -0400

    [GH-4] added city, country, and countrylanguages to app home page

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { Cities } from "./City";
+import { Countries } from "./Country";
+import { CountryLanguages } from "./CountryLanguage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql',
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div style={{
+      backgroundColor: '#00000008',
+      display: 'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      height: '100vh',
+      flexDirection: 'column',
+    }}>
+      <h2>Cities</h2>
+      <Cities />
+
+      <h2>Countries</h2>
+      <Countries />
+
+      <h2>Country Languages</h2>
+      <CountryLanguages />
     </div>
-  );
-}
-
+  </ApolloProvider>
+);
 export default App;
