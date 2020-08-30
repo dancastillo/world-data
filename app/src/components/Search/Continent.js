ed44d7f2a931ed35fbee0802e212c3f1bcfbb529
dancastillo <dancastillo99@gmail.com>
2020-08-30 13:05:10 -0400

    [GH-10] added search component with select and submit form

---
+++ app/src/components/Search/Continent.js
@@ -0,0 +1,53 @@
+import React, { useContext } from 'react';
+import { useQuery } from 'react-apollo';
+import { QUERY_CONTINENTS } from "../../helpers/query";
+import WorldContext from '../../context/world-context';
+
+const SearchContinent = () => {
+  const { data } = useQuery(QUERY_CONTINENTS);
+  const { setContinent } = useContext(WorldContext);
+
+  const defaultSelectValue = 'default';
+  const searchContinent = React.createRef();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const continentVal = (searchContinent.current.value).trim().toLowerCase();
+
+    if (continentVal === defaultSelectValue) {
+      return;
+    }
+
+    setContinent(continentVal);
+  };
+
+  return (
+      <div>
+        <form onSubmit={ handleSubmit }>
+          {
+            (data === undefined)
+              ? <p>Loading.....</p>
+              :(
+                <select
+                  name="searchContinent"
+                  id="searchContinent"
+                  ref={searchContinent}
+                  defaultValue={defaultSelectValue}
+                >
+                  <option disabled value={defaultSelectValue}>Select a Continent</option>
+                  {data.continents.map(({ continent }) => (
+                    <option value={continent.toLowerCase()} key={continent.toLowerCase()}>
+                      {continent}
+                    </option>
+                  ))};
+
+                </select>
+              )
+          }
+          <button type="submit">Find Continent</button>
+        </form>
+      </div>
+    );
+};
+
+export default SearchContinent;
